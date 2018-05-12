156911485f258fe6172256cee0f6579e9d23ab96
Rajat Varyani <rajat.varyani@outlook.com>
2018-05-13 00:31:34 +0530

    Fix CORS

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -15,7 +15,7 @@ func HomeHandler() http.HandlerFunc  {
 }
 
 func setupResponse(w *http.ResponseWriter, req *http.Request) {
-	(*w).Header().Set("Access-Control-Allow-Origin", "*")
-	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+	(*w).Header().Set("access-control-allow-origin", "*")
+	(*w).Header().Set("access-control-allow-methods", "POST, GET, OPTIONS, PUT, DELETE")
+	(*w).Header().Set("access-control-allow-headers", "accept, content-Type")
 }
