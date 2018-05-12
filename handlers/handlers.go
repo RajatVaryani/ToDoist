30ca250d1edfdf28a814ae8c8eda82b8aee42e9f
Rajat Varyani <rajat.varyani@outlook.com>
2018-05-13 00:19:51 +0530

    Enable CORS with OPTIONS

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -4,11 +4,18 @@ import "net/http"
 
 func HomeHandler() http.HandlerFunc  {
 	return func(writer http.ResponseWriter, request *http.Request) {
-		enableCors(&writer)
+		setupResponse(&writer, request)
+
+		if (*request).Method == "OPTIONS" {
+			return
+		}
+
 		writer.WriteHeader(http.StatusOK)
 	}
 }
 
-func enableCors(w *http.ResponseWriter) {
+func setupResponse(w *http.ResponseWriter, req *http.Request) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 }
