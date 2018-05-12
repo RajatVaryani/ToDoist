4218dad525876054feba06a1dc5ddad2da23785f
Rajat Varyani <rajat.varyani@outlook.com>
2018-05-13 00:14:29 +0530

    Enable CORS

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -4,6 +4,11 @@ import "net/http"
 
 func HomeHandler() http.HandlerFunc  {
 	return func(writer http.ResponseWriter, request *http.Request) {
+		enableCors(&writer)
 		writer.WriteHeader(http.StatusOK)
 	}
+}
+
+func enableCors(w *http.ResponseWriter) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
 }
